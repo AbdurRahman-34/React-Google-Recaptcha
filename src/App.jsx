e863c0bfc66b929e94b4e796341720448fd5ae7f
AbdurRahman-34 <get.abdurahman@gmail.com>
2023-05-19 23:25:47 +0600

    Recaptcha components add

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import ReCAPTCHA from "react-google-recaptcha";
 function App() {
-  const [count, setCount] = useState(0)
+
+function onChange(value) {
+  console.log("Captcha value:", value);
+}
 
   return (
     <>
-      
+       <ReCAPTCHA
+    sitekey="Your client site key"
+    onChange={onChange}
+  />
     </>
   )
 }
